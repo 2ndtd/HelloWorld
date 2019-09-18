bf17bdbee09c4bf3908c9c5935d10c74ea1d2eaa
2ndtd <itztoido@yahoo.com>
2019-09-17 23:55:26 -0400

    Added a button that is center horizontally and coded the button onclick to change the text attribute once clicked to another text.

---
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -0,0 +1,31 @@
+package edu.temple.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button buttonText;
+        buttonText = findViewById(R.id.button);
+
+        buttonText.setOnClickListener(
+                new Button.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        buttonText.setText("You have clicked the button");
+
+                    }
+                }
+        );
+
+    }
+}
